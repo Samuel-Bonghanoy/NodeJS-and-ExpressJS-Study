efa62125fe5a5e96a9d2d92d666c60a456fb1fdc
sethonne <20103261@usc.edu.ph>
2023-02-19 09:16:40 +0800

    initial commit

--- ExpressJS/controllers/tourController.js
+++ ExpressJS/controllers/tourController.js
@@ -29,8 +29,35 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //FILTERING
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    //ADVANCED FILTERING
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(
+      /\b(gte|gte|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+    console.log(JSON.parse(queryStr));
+
+    console.log(req.query);
+    const query = Tour.find(JSON.parse(queryStr));
+    // const query = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy',
+    // });
+
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty').equals('easy');
+
+    //EXECUTE QUERY
+    const tours = await query;
+
+    //send response
     res.status(200).json({
       status: 'success',
       results: tours.length,
